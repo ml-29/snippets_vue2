7bf45578e1e81845c6f20b9feba389ef3e1df0ac
ml-29 <melanie.loubet29@gmail.com>
2022-11-21 13:02:07 +0100

    Added Snippet Search component + migrated it from Vue 2 to Vue 3

---
+++ src/components/SnippetSearch.vue
@@ -0,0 +1,24 @@
+<script>
+import { useSnippetStore } from '@/stores/snippet.js'
+
+export default {
+	setup(){
+		const snippetStore = useSnippetStore;
+		return { snippetStore };
+	},
+	data(){
+		return {
+			search: ''
+		}
+	},
+	watch: {
+		search: function(s) {
+			this.snippetStore().searchSnippets(s);
+		}
+	}
+}
+</script>
+
+<template>
+	<input placeholder="Search" v-model="search">
+</template>
